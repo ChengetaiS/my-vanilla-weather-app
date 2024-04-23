21d88ae11b243a9ce132cb57407a77686ef7676d
ChengetaiS <162143946+ChengetaiS@users.noreply.github.com>
2024-04-23 23:03:05 +0200

    Updated weather search, utilizing an api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#actual-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "607bfb3coe77c132397e1ee35e4f2cat";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchingForCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchingForCity);
+
+searchCity("London");
